d445eec6933127ea7b10322c0d353563dfdb9386
Michelle <michelle.dutoit1994@gmail.com>
2019-11-02 21:49:40 +1100

    application working and illustrative background added

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers(burger_name, devoured)
+	values('Krabby Patty', false),
+    ('Flora-Burger', false),
+    ('Booger Burger', false),
+    ('Bama-Burger', false);
+    
+    select * from burgers;
